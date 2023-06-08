f81203c5d748db5ff2c446f1149238a67524ed36
Michael O’Brien <mobrien@cheekyghost.com>
2023-06-08 19:48:19 +1000

    Adding swift 5.8 package support

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SyntaxSparrow",
+    platforms: [.macOS(.v10_15)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SyntaxSparrow",
+            targets: ["SyntaxSparrow"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: Version("509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-06-05-a")),
+    ],
+    targets: [
+        .target(
+            name: "SyntaxSparrow",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ]
+        ),
+        .testTarget(
+            name: "SyntaxSparrowTests",
+            dependencies: [
+                "SyntaxSparrow",
+            ]
+        ),
+    ]
+)
+
+// Supplementary
+package.dependencies.append(contentsOf: [
+    .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0"),
+])

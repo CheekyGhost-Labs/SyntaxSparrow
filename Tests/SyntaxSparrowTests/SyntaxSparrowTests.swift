2d7685a32e98cdd86d3b0e23d23616366bbab79b
Michael O’Brien <mobrien@cheekyghost.com>
2023-06-11 14:13:55 +1000

    Finalised Declaration unit tests

--- Tests/SyntaxSparrowTests/SyntaxSparrowTests.swift
+++ Tests/SyntaxSparrowTests/SyntaxSparrowTests.swift
@@ -2,11 +2,5 @@
 import XCTest
 
 final class SyntaxSparrowTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documenation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+    // no-op
 }

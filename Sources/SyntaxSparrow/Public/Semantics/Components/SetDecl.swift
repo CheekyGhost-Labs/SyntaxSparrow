a55babfc31eac402395644e08bb87c40bff1c741
Michael O'Brien <98201889+mobrien-ghost@users.noreply.github.com>
2024-03-11 01:27:57 +1000

    Fixing incorrect optional description bug for effected semantic types (#36)
    * Fixing incorrect optional description bug for effected semantic types

--- Sources/SyntaxSparrow/Public/Semantics/Components/SetDecl.swift
+++ Sources/SyntaxSparrow/Public/Semantics/Components/SetDecl.swift
@@ -63,6 +63,7 @@ public struct SetDecl: Hashable, Equatable, CustomStringConvertible {
     // MARK: - CustomStringConvertible
 
     public var description: String {
-        resolver.node.description.trimmed
+        let base = resolver.node.description.trimmed
+        return base + (isOptional ? "?" : "")
     }
 }

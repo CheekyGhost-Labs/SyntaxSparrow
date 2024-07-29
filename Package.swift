ab392c3b486341a855323e9514631467e7f64b20
Michael O’Brien <mobrien@cheekyghost.com>
2024-07-30 07:43:26 +1000

    [CHORE] Switching from apple swift-syntax to swiftlang

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "510.0.1"),
     ],
     targets: [
         .target(

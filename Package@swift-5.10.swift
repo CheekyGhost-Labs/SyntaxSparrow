3a34c47baf97cb180f9c9661d90c478f3686b1e7
Michael O’Brien <mobrien@cheekyghost.com>
2024-10-23 17:06:57 +1000

    Adding dedicated swift 5.10 manifest

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SyntaxSparrow",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SyntaxSparrow",
+            targets: ["SyntaxSparrow"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
+    ],
+    targets: [
+        .target(
+            name: "SyntaxSparrow",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ],
+            resources: [
+                .copy("Resources/PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "SyntaxSparrowTests",
+            dependencies: [
+                "SyntaxSparrow",
+            ]
+        ),
+    ]
+)
+
+// Supplementary
+package.dependencies.append(contentsOf: [
+    .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0"),
+])
